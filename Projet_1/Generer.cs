1fb597bad6772620ba3ea9e7009409a45a860c31
GreatMartini <cmartinitrz@gmail.com>
2024-10-14 14:45:13 +0200

    On doit procéder au traitement

--- Projet_1/Generer.cs
+++ Projet_1/Generer.cs
@@ -20,24 +20,26 @@ namespace Projet_1{
             Random r_solde_aux = new Random();
             decimal r_solde;
             int r_cpt;
-            //if (!File.Exists("Comptes.csv")){
-                using (StreamWriter w = File.CreateText("Comptes.csv")){
-                    for (int i = 0; i < 20; i++){
-                        r_cpt = r_cpt_aux.Next(100);
-                        r_solde = (decimal)r_solde_aux.NextDouble()*2-1;
-                        w.WriteLine($"{r_cpt};{r_solde}");
-                    }
+            using (StreamWriter w = File.CreateText("Comptes.csv")){
+                for (int i = 0; i < 20; i++){
+                    r_cpt = r_cpt_aux.Next(100);
+                    r_solde = (decimal)r_solde_aux.NextDouble()*2-1;
+                    w.WriteLine($"{r_cpt};{r_solde}");
                 }
-            
-            //}
-
+            }
         }
         public static void Genere_transactions(){
             // Pour transactions
-            Random r_type_aux = new Random();
-            Random r_num_ind = new Random();
-            int num_aux;
-            Random r_montant_aux = new Random();
+            Random r_type_aux = new Random();                   // Type de la transaction
+            Random r_num_ind = new Random();                    // Indice pour les numeros de compte
+            Random r_montant_aux = new Random();                // Random pour le montant de la transaction
+            Random r_num_vir = new Random();                    // Random pour le num du virement
+
+            int num_ind_1;                                        // Auxiliaire pour l'indice
+            int num_ind_2;                                        // Auxiliaire pour l'indice
+            int num_aux;                                        // Auxiliaire pour le numero de transaction
+            decimal montant_aux;                                // Auxiliaire pour le montant
+
             List <int> numero_compte = new List<int>();
                 if (File.Exists("Comptes.csv")){
                     using (StreamReader r = new StreamReader("Comptes.csv")){
@@ -46,9 +48,7 @@ namespace Projet_1{
                             var values = line.Split(';');
                             num_aux = Convert.ToInt32(values[0]);
                             numero_compte.Add(num_aux);
-                        }
-                    
-                    //numero_compte = (int)numero_compte;
+                        }                    
                     }
                 }
                 else{
@@ -57,12 +57,16 @@ namespace Projet_1{
                 
             using (StreamWriter w = File.CreateText("Transactions.csv")){
                 for (int i = 0; i < 100; i++){
-                    //r_cpt = r_cpt_aux.Next(100);
-                    //r_solde = (decimal)r_solde_aux.NextDouble()*2-1;
+
+                    num_ind_1 = r_num_ind.Next(0,numero_compte.Count());      // On génère un indice aléatoire
+                    num_ind_2 = r_num_ind.Next(0,numero_compte.Count());      // On génère un indice aléatoire
+                    num_aux = r_num_vir.Next(0,10000);                        // Genere numéro de viremen aléatoire
+                    montant_aux = (decimal)r_montant_aux.NextDouble()*1000000;// Génère montant aléatoire
+
+                    w.WriteLine($"{num_aux};{montant_aux};{numero_compte[num_ind_1]};{numero_compte[num_ind_2]}");
+
                 }
             }
-
-
         }
     }
 }
--- Projet_1/obj/Debug/net8.0/Projet_1.AssemblyInfo.cs
+++ Projet_1/obj/Debug/net8.0/Projet_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ced71137bf1cff3bb21ff1af543c07687697ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c05ed675617432c5d359957a981ff93add68ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
